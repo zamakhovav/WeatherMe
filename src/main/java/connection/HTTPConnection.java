bed83a98828304352c840cad304e7be2f97682ed
nikasamuell <zamakhovav@gmail.com>
2017-10-15 11:28:54 +0300

    HTTPConnection is changed

--- src/main/java/connection/HTTPConnection.java
+++ src/main/java/connection/HTTPConnection.java
@@ -1,12 +1,30 @@
 package connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
-/**
- * Created by nikasamuell on 25/09/2017.
- */
 public class HTTPConnection {
-    public static HttpURLConnection makeItGetRequested(String urlLink) {
-        return null;
+    HttpURLConnection connection;
+
+    public String readResponseFromFile() throws IOException {
+        StringBuilder response = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            response.append(line + "\n");
+        }
+        return response.toString();
+    }
+
+    public static HTTPConnection makeItGetRequested(String urlLink) throws IOException {
+        HTTPConnection httpConnection = new HTTPConnection();
+        httpConnection.connection = (HttpURLConnection) new URL(urlLink).openConnection();
+        httpConnection.connection.connect();
+        return httpConnection;
     }
 }
