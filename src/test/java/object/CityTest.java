9ca07ce9f3f6541484da4b68724ead67c2794e9c
nikasamuell <zamakhovav@gmail.com>
2017-12-17 02:04:22 +0200

    city tests

---
+++ src/test/java/object/CityTest.java
@@ -0,0 +1,56 @@
+package object;
+
+import object.City;
+
+import object.Coordinates;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+public class CityTest {
+    private City city;
+
+    @Before
+    public void setUp() throws Exception {
+        city = new City("Tallinn", "EE", Coordinates.of(59.44, 24.75));
+    }
+
+    @Test
+    public void testRequestToLatitude() {
+        try {
+            assertEquals("24.75", city.getCoordinates().toStringOfLatitude());
+        } catch (Exception e) {
+            fail("Failure was caused by " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testRequestToLongitude() {
+        try {
+            assertEquals("59.44", city.getCoordinates().toStringOfLongitude());
+        } catch (Exception e) {
+            fail("Failure was caused by " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testRequestToCity() {
+        try {
+            assertEquals("Tallinn", city.getCityName());
+        } catch (Exception e) {
+            fail("Failure was caused by " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testRequestToCityCode() {
+        try {
+            assertEquals("EE", city.getCountryCode());
+        } catch (Exception e) {
+            fail("Failure was caused by " + e.getMessage());
+        }
+    }
+}
